aba6fe73dd65056b9147bbd12b9f8627ff346232
meryemunaldi <unaldimeryem747@gmail.com>
2026-02-13 20:36:40 +0300

    Add files via upload

---
+++ Simple Shell.c
@@ -0,0 +1,159 @@
+/*#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<direct.h>
+
+#define MAX_LINE 80
+
+int main(void){
+	char input[MAX_LINE];
+	int should_run = 1;
+	char *args[10];
+	
+	while(should_run){
+		char cwd[1024];
+		if(getcwd(cwd, sizeof(cwd)) != NULL){
+			printf("Mery_Shell[%s]> ", cwd);
+		}else{
+			printf("Mery_Shell> ");
+		}
+		fflush(stdout);
+		
+		if(fgets(input, MAX_LINE, stdin) == NULL) break;
+		input[strcspn(input, "\n")] = 0;
+		
+		char *token;
+int i =0;
+
+token = strtok(input, " ");
+while(token != NULL){
+	args[i] = token;
+	i++;
+	token = strtok(NULL, " ");
+}
+args[i] = NULL;
+	if(i>0){
+
+		if(strcmp(input, "exit")==0){
+			should_run =0;
+			continue;
+		}
+		if(strlen(input)>0){
+			if(strcmp(args[0], "hakkinda") ==0){
+				printf("\n-- Mery_Shell Info--\n");
+				printf("Developer: Meryem Ünaldý\n");
+				printf("Bu shell projesi C dili ile sistem programlama temellerini ogrenmek icin yapilmistir.\n\n");
+			} 
+			else if(strcmp(args[0], "topla") == 0){
+				if(args[1] != NULL && args[2] != NULL){
+					int sayi1 = atoi(args[1]);
+					int sayi2 = atoi(args[2]);
+					printf("Sonuc: %d\n",sayi1 + sayi2);
+				}else{
+					printf("Hata: Lütfen iki sayi girin.(Ornek topla 5 10)\n");
+				}
+			}
+			else if(strcmp(args[0],"cd") == 0){
+				if(args[1] == NULL){
+					printf("Hata: Gitmek istediginiz dizini belirtin.\n");
+				}else{
+					if(chdir(args[1])!=0){
+						perror("Mery_Shell Hata");
+					}else{
+						printf("Dizin degistirildi.\n");
+					}
+				}
+			}
+		else{
+			system(input);
+		}		
+	}
+		if(strlen(input)>0){
+			printf("Komut yurutuluyor...\n");
+			system(input);
+		}
+	}
+	}
+	return 0;
+}
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <direct.h>
+
+#define MAX_LINE 80
+
+int main(void) {
+    char input[MAX_LINE];
+    int should_run = 1;
+    char *args[10];
+
+    while (should_run) {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("Mery_Shell[%s]> ", cwd);
+        } else {
+            printf("Mery_Shell> ");
+        }
+        fflush(stdout);
+
+        if (fgets(input, MAX_LINE, stdin) == NULL) break;
+        input[strcspn(input, "\n")] = 0;
+
+        // --- DÜZELTME: Parçalama iþlemi tam burada, karþýlaþtýrmadan önce olmalý ---
+        int i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && i < 9) {
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL; // Dizinin sonunu iþaretle
+
+        if (i > 0) { // Eðer kullanýcý bir þey yazdýysa
+            
+            // 1. Çýkýþ kontrolü
+            if (strcmp(args[0], "exit") == 0) {
+                should_run = 0;
+                continue;
+            }
+            
+            // 2. Hakkýnda komutu
+            if (strcmp(args[0], "hakkinda") == 0) {
+                printf("\n-- Mery_Shell Info --\n");
+                printf("Developer: Meryem Unaldi\n");
+                printf("Bu shell projesi C dili ile sistem programlama temellerini ogrenmek icin yapilmistir.\n\n");
+            } 
+            // 3. Toplama komutu
+            else if (strcmp(args[0], "topla") == 0) {
+                if (args[1] != NULL && args[2] != NULL) {
+                    int sayi1 = atoi(args[1]);
+                    int sayi2 = atoi(args[2]);
+                    printf("Sonuc: %d\n", sayi1 + sayi2);
+                } else {
+                    printf("Hata: Lutfen iki sayi girin. (Ornek: topla 5 10)\n");
+                }
+            }
+            // 4. CD komutu
+            else if (strcmp(args[0], "cd") == 0) {
+                if (args[1] == NULL) {
+                    printf("Hata: Gitmek istediginiz dizini belirtin.\n");
+                } else {
+                    if (chdir(args[1]) != 0) {
+                        perror("Mery_Shell Hata");
+                    } else {
+                        printf("Dizin degistirildi.\n");
+                    }
+                }
+            }
+            // 5. Bilinmeyen komutlarý sisteme gönder
+            else {
+                system(input);
+            }
+        }
+    }
+
+    return 0;
+}
